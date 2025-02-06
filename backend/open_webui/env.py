f3788ed4108a99899ca1d1fc1a062dd009bae721
sdougherty19 <sdougherty19@gmail.com>
2025-02-06 10:23:52 -0500

    Update env.py
    Branding

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,11 +106,11 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Logic System AI")
+if WEBUI_NAME != "Logic System AI":
+    WEBUI_NAME += " (Logic System AI)"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://logicsystem.ai/favicon.png"
 
 
 ####################################
