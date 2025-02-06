6aac55ccf16fce9acf0688a7843f7ce13fc954f6
sdougherty19 <sdougherty19@gmail.com>
2025-02-06 11:14:07 -0500

    Update +page.svelte

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -199,7 +199,7 @@
 									<div class=" mt-1 text-xs font-medium text-gray-500">
 										ⓘ {$WEBUI_NAME}
 										{$i18n.t(
-											'does not make any external connections, and your data stays securely on your locally hosted server.'
+											'keeps your data safe and isolated. We do not use your data in any way, except to provide you this service.'
 										)}
 									</div>
 								{/if}
