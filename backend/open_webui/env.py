282bc3b0833a82acfcc32be55e2a29610a2aca01
sdougherty19 <sdougherty19@gmail.com>
2025-02-09 13:03:38 -0500

    Update env.py
    Version update

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -122,12 +122,13 @@ ENV = os.environ.get("ENV", "dev")
 FROM_INIT_PY = os.environ.get("FROM_INIT_PY", "False").lower() == "true"
 
 if FROM_INIT_PY:
-    PACKAGE_DATA = {"version": importlib.metadata.version("open-webui")}
+   # PACKAGE_DATA = {"version": importlib.metadata.version("open-webui")}
+    PACKAGE_DATA = {"version": "1.0.0"}
 else:
     try:
         PACKAGE_DATA = json.loads((BASE_DIR / "package.json").read_text())
     except Exception:
-        PACKAGE_DATA = {"version": "0.0.0"}
+        PACKAGE_DATA = {"version": "1.0.0"}
 
 
 VERSION = PACKAGE_DATA["version"]
